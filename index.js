ccb6cc12cf27b0324732a54b4f2bcff424f56faa
teddy cubaka <teddycubaka96@gmail.com>
2023-01-17 20:38:52 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const propretyRouter = require("./routes/propreties");
+
+const app = express();
+
+mongoose
+	.connect("mongodb://localhost:27017/zubu", {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log("Connexion à MongoDB réussie !"))
+	.catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use(bodyParser.json());
+
+app.use("/api", (req, res, next) => {
+	res.send({
+		message:
+			"Welcome to zubu, Take contact with Teddy, my King, to more information",
+	});
+});
+
+app.listen("4000", () => {
+	console.log("listen on the port 4000");
+});
