f6d1b7cda09f8fa3163ecbef0c732a41b1868f0a
teddy cubaka <teddycubaka96@gmail.com>
2023-01-18 10:32:53 +0100

    Create the controllers of propreties

---
+++ controllers/propreties.js
@@ -0,0 +1,36 @@
+const Proprety = require("../models/propreties");
+
+exports.addProprety = (req, res) => {
+	const proprety = new Proprety({ ...req.body });
+	proprety
+		.save()
+		.then((data) => res.send({ message: "Propreties added", data }))
+		.catch((err) => res.send(err));
+};
+
+exports.getPropreties = (req, res) => {
+	Proprety.find({ _id: { $in: req.body.propreties } })
+		.then((data) => res.send(data))
+		.catch((err) => res.send(err));
+};
+
+exports.getAllPropreties = (req, res) => {
+	Proprety.find()
+		.then((data) => res.send(data))
+		.catch((err) => res.send(err));
+};
+
+exports.getThreeToper = (req, res) => {
+	Proprety.find()
+		.then((data) => {
+			data.sort((a, b) => (a.referencing_note > b.referencing_note ? 1 : -1));
+			res.send({ propreties_on_top: [data[0], data[1], data[2]] });
+		})
+		.catch((err) => res.send(err));
+};
+
+exports.updateProprety = (req, res) => {
+	Proprety.updateOne({ _id: req.body.discussionId }, { ...req.body })
+		.then((data) => res.send(data))
+		.catch((err) => res.send(err));
+};
