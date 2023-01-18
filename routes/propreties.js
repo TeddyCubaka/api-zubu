39f04a1f47fb1b40d397f2ceae526b1b3990fd04
teddy cubaka <teddycubaka96@gmail.com>
2023-01-18 10:40:39 +0100

    Create the routes/propreties file for routing of propreties

---
+++ routes/propreties.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+const propretiesCtrl = require("../controllers/propreties");
+
+router.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+	);
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET, POST, PUT, DELETE, PATCH, OPTIONS"
+	);
+	next();
+});
+
+router.post("/", propretiesCtrl.addProprety);
+router.get("/", propretiesCtrl.getAllPropreties);
+router.get("/three_On_top", propretiesCtrl.getThreeToper);
+router.post("/:id", propretiesCtrl.updateProprety);
