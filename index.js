80d5974333b5dbbfda803264389fbee6f80b90a4
teddy cubaka <teddycubaka96@gmail.com>
2023-04-03 03:16:43 +0100

    Update the index.js file to deploy the server on vercel

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 
-const PropretyRoute = require("./routes/propreties");
-const UserRoute = require("./routes/users");
+const PropretyRoute = require("./api/routes/propreties");
+const UserRoute = require("./api/routes/users");
 
 const app = express();
 
@@ -19,6 +19,9 @@ mongoose
 app.use(bodyParser.json());
 
 app.use(express.static("public"));
+app.get("/", (req, res) => {
+	res.sendFile("index.html", { root: path.join(__dirname, "public") });
+});
 app.use("/api/proprety", PropretyRoute);
 app.use("/api/user", UserRoute);
 app.use("/api", (req, res, next) => {
@@ -27,10 +30,7 @@ app.use("/api", (req, res, next) => {
 			"Welcome to zubu, Take contact with Teddy, my King, to more information",
 	});
 });
-//index.js
-app.get("/", (req, res) => {
-	res.sendFile("index.html", { root: path.join(__dirname, "public") });
-});
+
 
 app.listen("4000", () => {
 	console.log("listen on http://localhost:4000");
